f7a80a659cfe0d76dcb1a0b5e63e7786b44ad2ab
Xun Zhang <59300821+xz-cs@users.noreply.github.com>
2021-09-10 14:23:42 +0800

    Add member

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         member.add("Alex_XU");
+        member.add("zx");
         // To-do 
         // member.add("Your_Name_Here!");
 
