14cfa4f616a939f89c7a1e8107eb88b073f77e85
xujiakai <jiakaixu2-c@my.cityu.edu.hk>
2021-10-06 12:47:05 +0800

    Temporarily comment unreadable characters

--- MetroSystem/src/metroSystem/Main.java
+++ MetroSystem/src/metroSystem/Main.java
@@ -5,26 +5,28 @@ import java.util.ArrayList;
 public class Main
 {
    public static void main(String[] args) {
-       MetroSystem m = MetroSystem.getInstance();
-       m.setSystemLanguage(LanguageSimplifiedChinese.getInstance());
+        MetroSystem m = MetroSystem.getInstance();
+        m.setSystemLanguage(LanguageSimplifiedChinese.getInstance());
 
-       Line testLine = m.getDatabase().getLineByName("–|èF¾€", LanguageTraditionalChinese.getInstance());
-       System.out.println(testLine.getName());
-       ArrayList<Edge> edges = testLine.getEdges();
-       for (Edge e : edges)
-           System.out.println(e.toString());
-       System.out.println();
+        // Commented out due to character cannot be resolved
+        // Line testLine = m.getDatabase().getLineByName("ï¿½|ï¿½Fï¿½ï¿½", LanguageTraditionalChinese.getInstance());
+        // System.out.println(testLine.getName());
+        // ArrayList<Edge> edges = testLine.getEdges();
+        // for (Edge e : edges)
+        //     System.out.println(e.toString());
+        // System.out.println();
 
-       Station testStation = m.getDatabase().getStationByName("¾ÅýˆÌÁ", LanguageTraditionalChinese.getInstance(), AdministratorHK.getInstance());
-       System.out.println(testStation.getName());
-       edges = testStation.getEdgeTo();
-       for (Edge e : edges) {
-           System.out.println(e.toString() + ' ' + e.getLine());
-       }
-       
-       Criteria c = new CrtStation();
-       ArrayList<Integer> result = c.findRoute(5, 88);
-       System.out.println(result);
+        // Commented out due to character cannot be resolved
+        // Station testStation = m.getDatabase().getStationByName("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½", LanguageTraditionalChinese.getInstance(), AdministratorHK.getInstance());
+        // System.out.println(testStation.getName());
+        // edges = testStation.getEdgeTo();
+        // for (Edge e : edges) {
+        //     System.out.println(e.toString() + ' ' + e.getLine());
+        // }
+
+        Criteria c = new CrtStation();
+        ArrayList<Integer> result = c.findRoute(5, 88);
+        System.out.println(result);
    }
    
 }
