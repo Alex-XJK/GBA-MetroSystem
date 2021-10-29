3fb80e1a143180ddc0972791e8fe123a56bcf573
xrr <ruixu33-c@my.cityu.edu.hk>
2021-10-29 21:54:23 +0800

    Refill main

---
+++ Source/MetroSystem/src/metroSystem/ExStationNotFound.java
@@ -0,0 +1,12 @@
+package metroSystem;
+
+/**
+ * Exception class.
+ * Throws when the desired station cannot be found
+ */
+public class ExStationNotFound extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public ExStationNotFound() { super("Station you look for does not exist."); }
+    public ExStationNotFound(String message) { super(message); }
+}
