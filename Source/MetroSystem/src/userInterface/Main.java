8996a5568e7902bee88d0b9aa41d81135ffbb09e
Xun Zhang <59300821+xz-cs@users.noreply.github.com>
2021-11-05 23:52:26 +0800

    Add support for multi-language; add reset function

--- Source/MetroSystem/src/userInterface/Main.java
+++ Source/MetroSystem/src/userInterface/Main.java
@@ -18,6 +18,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        Stack<String> lang = new Stack<>();
+        lang.push("en");
+        Stack<String> area = new Stack<>();
+        area.push("hk");
+
         // Theme parity
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
@@ -32,7 +37,7 @@ public class Main {
         LocalDateTime now = LocalDateTime.now();
         // System.out.println(dtf.format(now));
 
-        JFrame frame=new JFrame("Traffic Map");
+        JFrame frame = new JFrame("Traffic Map");
 
         JPanel sideBar = new JPanel();
         sideBar.setBounds(0, 0, 180, 1000);
@@ -41,6 +46,7 @@ public class Main {
         JButton langcn = new JButton("简体中文");
         JButton langhk = new JButton("繁體中文");
         JButton langen = new JButton("English");
+        JButton reset = new JButton("Reset");
         JButton btnExit = new JButton("Exit");
         JLabel user = new JLabel("<html><h1>Jacky</h1><p>Registered user</p><br></html>");
 
@@ -58,10 +64,10 @@ public class Main {
         // sideBar.add(profileLabel);
         sideBar.setLayout(new BoxLayout(sideBar, BoxLayout.Y_AXIS));
         sideBar.add(user);
-        sideBar.add(btnMap);
         sideBar.add(langcn);
         sideBar.add(langhk);
         sideBar.add(langen);
+        sideBar.add(reset);
         sideBar.add(btnExit);
 
         frame.add(sideBar);
@@ -95,7 +101,6 @@ public class Main {
         JButton btnsz = new JButton("Traffic Map (SZ)");
 
         JButton find_path = new JButton("Find Path");
-        JButton clear = new JButton("Clear");
 
         // jp.add(username);
         // jp.add(picLabel);
@@ -104,23 +109,21 @@ public class Main {
         // Szbutton mtr_sz = new Szbutton();
         Map mtr_en_hk = new Map("en", "HK");
         mtr_en_hk.linkTextFieldComponent(from_text, to_text);
-        // ArrayList<JButton> szButtons = mtr_en_hk./getAllButtons();
-        /*for(JButton b: szButtons) {
-            b.addActionListener(new ActionListener() {
-                                    public void actionPerformed(ActionEvent e) {
-                                        if(from_text.getText().equals("")){
-                                            from_text.setText(((JButton) (e.getSource())).getName());
-                                        }
-                                        else{
-                                            to_text.setText(((JButton) (e.getSource())).getName());
-                                        }
-                                    };
-                                }
-            );
-        }*/
 
         Map mtr_en_sz = new Map("en", "SZ");
         mtr_en_sz.linkTextFieldComponent(from_text, to_text);
+
+        Map mtr_hk_hk = new Map("hk", "HK");
+        mtr_hk_hk.linkTextFieldComponent(from_text, to_text);
+
+        Map mtr_hk_sz = new Map("hk", "SZ");
+        mtr_hk_sz.linkTextFieldComponent(from_text, to_text);
+
+        Map mtr_ch_hk = new Map("ch", "HK");
+        mtr_ch_hk.linkTextFieldComponent(from_text, to_text);
+
+        Map mtr_ch_sz = new Map("ch", "SZ");
+        mtr_ch_sz.linkTextFieldComponent(from_text, to_text);
 
         Image sz_bg = ImageIO.read(new File(base + "/data/sz.png"));
         // BackgroundPanel mtr_sz_bg = new BackgroundPanel(sz_bg, 2);
@@ -139,9 +142,14 @@ public class Main {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // picLabel.setIcon(szimg);
+                area.push("sz");
                 contentPane.removeAll();
-                contentPane.add(mtr_en_sz);
+                if (lang.peek().equals("en"))
+                    contentPane.add(mtr_en_sz);
+                else if (lang.peek().equals("hk"))
+                    contentPane.add(mtr_hk_sz);
+                else if (lang.peek().equals("ch"))
+                    contentPane.add(mtr_ch_sz);
                 contentPane.revalidate();
                 contentPane.repaint();
             }
@@ -151,8 +159,14 @@ public class Main {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                area.push("hk");
                 contentPane.removeAll();
-                contentPane.add(mtr_en_hk);
+                if (lang.peek().equals("en"))
+                    contentPane.add(mtr_en_hk);
+                else if (lang.peek().equals("hk"))
+                    contentPane.add(mtr_hk_hk);
+                else if (lang.peek().equals("ch"))
+                    contentPane.add(mtr_ch_hk);
                 contentPane.revalidate();
                 contentPane.repaint();
             }
@@ -174,10 +188,20 @@ public class Main {
                 String to_str = to_text.getText();
                 System.out.println(from_str + " --> " + to_str);
                 try {
-                    startStation = m.getDatabase().getStationByName(from_str, Language.English, AdministratorHK.getInstance());
+                    if (lang.peek().equals("en"))
+                        startStation = m.getDatabase().getStationByName(from_str, Language.English, AdministratorHK.getInstance());
+                    else if (lang.peek().equals("hk"))
+                        startStation = m.getDatabase().getStationByName(from_str, Language.TraditionalChinese, AdministratorHK.getInstance());
+                    else if (lang.peek().equals("ch"))
+                        startStation = m.getDatabase().getStationByName(from_str, Language.SimplifiedChinese, AdministratorHK.getInstance());
                 } catch (ExStationNotFound ex) {
                     try {
-                        startStation = m.getDatabase().getStationByName(from_str, Language.English, AdministratorSZ.getInstance());
+                        if (lang.peek().equals("en"))
+                            startStation = m.getDatabase().getStationByName(from_str, Language.English, AdministratorSZ.getInstance());
+                        else if (lang.peek().equals("hk"))
+                            startStation = m.getDatabase().getStationByName(from_str, Language.TraditionalChinese, AdministratorSZ.getInstance());
+                        else if (lang.peek().equals("ch"))
+                            startStation = m.getDatabase().getStationByName(from_str, Language.SimplifiedChinese, AdministratorSZ.getInstance());
                     } catch (ExStationNotFound exc) {
                         startStation = null;
                         exc.printStackTrace();
@@ -185,10 +209,20 @@ public class Main {
                 }
 
                 try {
-                    endStation = m.getDatabase().getStationByName(to_str, Language.English, AdministratorHK.getInstance());
+                    if (lang.peek().equals("en"))
+                        endStation = m.getDatabase().getStationByName(to_str, Language.English, AdministratorHK.getInstance());
+                    else if (lang.peek().equals("hk"))
+                        endStation = m.getDatabase().getStationByName(to_str, Language.TraditionalChinese, AdministratorHK.getInstance());
+                    else if (lang.peek().equals("ch"))
+                        endStation = m.getDatabase().getStationByName(to_str, Language.SimplifiedChinese, AdministratorHK.getInstance());
                 } catch (ExStationNotFound ex) {
                     try {
-                        endStation = m.getDatabase().getStationByName(to_str, Language.English, AdministratorSZ.getInstance());
+                        if (lang.peek().equals("en"))
+                            endStation = m.getDatabase().getStationByName(to_str, Language.English, AdministratorSZ.getInstance());
+                        else if (lang.peek().equals("hk"))
+                            endStation = m.getDatabase().getStationByName(to_str, Language.TraditionalChinese, AdministratorSZ.getInstance());
+                        else if (lang.peek().equals("ch"))
+                            endStation = m.getDatabase().getStationByName(to_str, Language.SimplifiedChinese, AdministratorSZ.getInstance());
                     } catch (ExStationNotFound exc) {
                         endStation = null;
                         exc.printStackTrace();
@@ -208,7 +242,12 @@ public class Main {
                     System.out.println("This route has passed Hong Kong region. Points are: ");
                     System.out.println(hkres);
                     try {
-                        mtr_en_hk.showResult(hkres);
+                        if (lang.peek().equals("en"))
+                            mtr_en_hk.showResult(hkres);
+                        else if (lang.peek().equals("hk"))
+                            mtr_hk_hk.showResult(hkres);
+                        else if (lang.peek().equals("ch"))
+                            mtr_ch_hk.showResult(hkres);
                     } catch (Exception ignored) {
                     }
                 }
@@ -216,7 +255,12 @@ public class Main {
                     System.out.println("This route has passed Shen Zhen region. Points are: ");
                     System.out.println(szres);
                     try {
-                        mtr_en_sz.showResult(szres);
+                        if (lang.peek().equals("en"))
+                            mtr_en_sz.showResult(szres);
+                        else if (lang.peek().equals("hk"))
+                            mtr_hk_sz.showResult(szres);
+                        else if (lang.peek().equals("ch"))
+                            mtr_ch_sz.showResult(szres);
                     } catch (Exception ignored) {
                     }
                 }
@@ -241,18 +285,19 @@ public class Main {
 
         btnExit.setBackground(Color.RED);
         find_path.setBackground(Color.GREEN);
-        clear.setBackground(Color.YELLOW);
+        reset.setBackground(Color.YELLOW);
 
         jp.add(btnsz);
         jp.add(find_path);
         // jp.add(find_path);
-        jp.add(clear);
+        // jp.add(reset);
 
         // Language settings
         langcn.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                lang.push("ch");
                 btnMap.setText("地图");
                 btnExit.setText("退出");
                 from.setText("起始地");
@@ -261,6 +306,16 @@ public class Main {
                 btnsz.setText("深圳地铁线路图");
                 find_path.setText("查找路径");
                 user.setText("<html><h1>Jacky</h1><p>注册用户</p><br></html>");
+                reset.setText("重设");
+                contentPane.removeAll();
+                if (area.peek().equals("sz"))
+                    contentPane.add(mtr_ch_sz);
+                else if (area.peek().equals("hk"))
+                    contentPane.add(mtr_ch_hk);
+                contentPane.revalidate();
+                contentPane.repaint();
+                from_text.setText("");
+                to_text.setText("");
             }
         });
 
@@ -268,6 +323,7 @@ public class Main {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                lang.push("hk");
                 btnMap.setText("地圖");
                 btnExit.setText("退出");
                 from.setText("起始地");
@@ -276,6 +332,16 @@ public class Main {
                 btnsz.setText("深圳地鐵線路圖");
                 find_path.setText("查找路徑");
                 user.setText("<html><h1>Jacky</h1><p>註冊用戶</p><br></html>");
+                reset.setText("重設");
+                contentPane.removeAll();
+                if (area.peek().equals("sz"))
+                    contentPane.add(mtr_hk_sz);
+                else if (area.peek().equals("hk"))
+                    contentPane.add(mtr_hk_hk);
+                contentPane.revalidate();
+                contentPane.repaint();
+                from_text.setText("");
+                to_text.setText("");
             }
         });
 
@@ -283,6 +349,7 @@ public class Main {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                lang.push("en");
                 btnMap.setText("Map");
                 btnExit.setText("Exit");
                 from.setText("From: ");
@@ -291,15 +358,40 @@ public class Main {
                 btnsz.setText("Traffic Map (SZ)");
                 find_path.setText("Find Path");
                 user.setText("<html><h1>Jacky</h1><p>Registered user</p><br></html>");
+                reset.setText("Reset");
+                contentPane.removeAll();
+                if (area.peek().equals("sz"))
+                    contentPane.add(mtr_en_sz);
+                else if (area.peek().equals("hk"))
+                    contentPane.add(mtr_en_hk);
+                contentPane.revalidate();
+                contentPane.repaint();
+                from_text.setText("");
+                to_text.setText("");
             }
         });
 
-        clear.addActionListener(new ActionListener() {
+        reset.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
+                lang.push("en");
+                area.push("hk");
                 from_text.setText("");
                 to_text.setText("");
+                contentPane.removeAll();
+                contentPane.add(mtr_en_hk);
+                contentPane.revalidate();
+                contentPane.repaint();
+                btnMap.setText("Map");
+                btnExit.setText("Exit");
+                from.setText("From: ");
+                to.setText("To: ");
+                btnhk.setText("Traffic Map (HK)");
+                btnsz.setText("Traffic Map (SZ)");
+                find_path.setText("Find Path");
+                user.setText("<html><h1>Jacky</h1><p>Registered user</p><br></html>");
+                reset.setText("Reset");
             }
         });
 
