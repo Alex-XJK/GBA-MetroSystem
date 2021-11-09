400533dbd51fc43b9cbd17f8ea01ce5422fcda7a
xrr <ruixu33-c@my.cityu.edu.hk>
2021-11-09 17:15:14 +0800

    Refactoring on main

--- Source/MetroSystem/src/metroSystem/MetroSystem.java
+++ Source/MetroSystem/src/metroSystem/MetroSystem.java
@@ -32,4 +32,80 @@ public class MetroSystem
     public Database getDatabase() {
         return database;
     }
+
+    /**
+     * Get all edges in a line.
+     * @return  All edges in a line
+     * @since   Nov. 9, 2021
+     */
+    public void getAllEdgesInALine(String name, Language language) {
+        Line testLine = null;
+        try {
+            testLine = database.getLineByName(name, language);
+            System.out.println(testLine.getName());
+            ArrayList<Edge> edges = testLine.getEdges();
+            for (Edge e : edges)
+                System.out.println(e.toString());
+            System.out.println();
+        } catch (ExLineNotFound e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Get all edges in a station.
+     * @return  All edges in a station
+     * @since   Nov. 9, 2021
+     */
+    public void getAllEdgesInAStation(String name, Language language, Administrator admin) {
+        Station testStation = null;
+        try {
+            testStation = database.getStationByName(name, language, admin);
+            System.out.println(testStation.getName());
+            ArrayList<Edge> edges = testStation.getEdgeTo();
+            for (Edge e : edges) {
+                System.out.println(e.toString() + ' ' + e.getLine());
+            }
+            System.out.println();
+        } catch (ExStationNotFound e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Station getStationByName(String name, Language language, Administrator admin) {
+        try {
+            return database.getStationByName(name, language, admin);
+        } catch (ExStationNotFound e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void computePrice(Station st_Station, Station ed_Station) {
+        // Demo of "Price" function
+        if(st_Station == null || ed_Station == null)
+            return;
+        Station startStation = st_Station;
+        Station endStation = ed_Station;
+
+        Station.getStationPrice(startStation, endStation);
+    }
+
+    public void computeShortestPath(Station st_Station, Station ed_Station, Criteria myCriteria) {
+        if(st_Station == null || ed_Station == null)
+            return;
+        Station startStation2 = st_Station;
+        Station endStation2 = ed_Station;
+
+        //Declare new criteria
+        Criteria criteria = myCriteria;
+        System.out.println(criteria);
+
+        //Compute the shortest path
+        ArrayList<Integer> result2 = criteria.findRoute(startStation2.getId(), endStation2.getId());
+
+        //Print out the final result
+        ArrayList<String> nameResult2 = database.translateId2Name(result2);
+        System.out.println(nameResult2);
+    }
 }
